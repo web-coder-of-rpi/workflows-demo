be3152fa08841ad351c39b494300f29dbb8026fd
web-coder-of-rpi <leo.jarnicki@icloud.com>
2025-08-22 18:26:46 +0800

    Update runccode.py

--- runccode.py
+++ runccode.py
@@ -1,3 +1,3 @@
 import subprocess
-result = subprocess.run(['./dist/hello_contrib'], capture_output=True, text=True)
+result = subprocess.run(['./hello_contrib'], capture_output=True, text=True)
 print(result.stdout)
